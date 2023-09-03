455ce466f48af5c65d654884bf38ee1d58a57540
ANASTASIA LUKINA <a.lukina.art@gmail.com>
2023-09-03 22:44:55 +0200

    added prometheus

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.boot:spring-boot-starter-web:3.0.2")
+    implementation("org.springframework.boot:spring-boot-starter-actuator:3.1.3")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.11.3")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
